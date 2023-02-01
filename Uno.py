fa2af8f65d27455856c7de791ddda450d75a56fd
DarrowWolf <47889966+DarrowWolf@users.noreply.github.com>
2023-02-01 16:06:01 +0800

    Added discard pile code and tidy up debug code

--- Uno.py
+++ Uno.py
@@ -35,39 +35,86 @@ class Player:
         self.name = name
         self.hand = []
 
+class DiscardPile:
+    def __init__(self):
+        self.pile = []
+
+    def add_to_pile(self, card):
+        self.pile.append(card)
+
 # Create a list of players
 players = [Player("Player 1"), Player("Player 2"), Player("Player 3"), Player("Player 4")]
 
 # Create a deck of cards
 deck = Deck.create_deck()
-print(deck) #prints created deck
-
-print()
-print()
-print()
-print()
-
-# Shuffle the deck of cards
-Deck.shuffle_deck(deck)
-print(Deck.shuffle_deck(deck)) #prints shuffled deck
-
-print()
-print()
-print()
-print()
-
-# Deal cards to players
-players = Deck.deal_cards(deck, players)
-
-# Print the players' hands
-for player in players:
-    print(f'{player.name} has the following cards:')
-    for card in player.hand:
-        print(f'\t{card}')
-    print()
 
 debug = True
-cardValCounts = [deck.count(val) for val in cardVal]
 if debug:
-    print(f'Card value counts: {cardValCounts}')
-    print(f'Cumulative card value counts: {np.cumsum(cardValCounts)}')
+    print(deck) #prints created deck
+
+    print()
+    print()
+
+    # Shuffle the deck of cards
+    Deck.shuffle_deck(deck)
+    print(Deck.shuffle_deck(deck)) #prints shuffled deck
+
+    print()
+    print()
+    # Deal cards to players
+    players = Deck.deal_cards(deck, players)
+
+    # Print the players' hands
+    for player in players:
+        print(f'{player.name} has the following cards:')
+        for card in player.hand:
+            print(f'\t{card}')
+        print()
+
+    print()
+    print()
+
+    discard_pile = DiscardPile()
+
+    # Example test of the discard pile
+    discarded_card = players[0].hand.pop()
+    discard_pile.add_to_pile(discarded_card)
+
+    print(f'{players[0].name} discarded {discarded_card}')
+    print(f'Discard pile: {discard_pile.pile}')
+
+    print()
+    print()
+
+    # Print the chosen player's hand
+    print(f'{players[0].name} has the following cards:')
+    for card in players[0].hand:
+        print(f'\t{card}')
+    print()
+    
+
+else:
+    print("Change Debug setting from \"False\" to \"True\" if you want to see debug output")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#This is such a joke code xd cumsum~ cum cum
+#cardValCounts = [deck.count(val) for val in cardVal]
+#if debug:
+#    print(f'Card value counts: {cardValCounts}')
+#    print(f'Cumulative card value counts: {np.cumsum(cardValCounts)}')
